a40cdc8a81bdfbedd2af59bdf3a8a7153bd29c50
poxstone <poxstone@gmail.com>
2015-10-22 03:39:14 +0000

    database created

---
+++ server.js
@@ -0,0 +1,67 @@
+// Get the packages we need
+var express = require('express');
+var mongoose = require('mongoose');
+// Call schema model
+var Beer = require('./models/beer');
+//parser for mongo
+var bodyParser = require('body-parser');
+
+// Connect to the beerlocker MongoDB
+mongoose.connect('mongodb://localhost:27017/beerlocker');
+
+// Create our Express application
+var app = express();
+
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Use environment defined port or 3000
+var port = process.env.PORT || 443;
+
+
+
+// Create our Express router
+var router = express.Router();
+
+// Initial dummy route for testing
+// http://localhost:3000/api
+router.get('/', function(req, res) {
+  res.json({ message: 'You are running dangerously low on beer!' });
+});
+
+// create a new route with the prefix /beers
+var beersRoute = router.route('/beers');
+
+//create endpoint /api/beers for POSTS
+beersRoute.post(function(req, res){
+    
+  // Create a new instace of Beer model
+  var beer = new Beer();
+  
+  // Set the beer properties that came from the POST data
+  beer.name = req.body.name;
+  beer.type = req.body.type;
+  beer.quantity = req.body.quantity;
+  
+  console.log('post: ', req.body );
+  
+  // Save the beer and check for errors
+  beer.save(function(err){
+    if (err)
+      res.send(err);
+    
+    res.json({ message: 'Beer added to the locker!', data: beer });
+  });
+    
+});
+
+// Register all our routes with /api
+app.use('/api', router);
+
+
+
+// Start the server
+app.listen(port);
+console.log('Insert beer on port ' + port);
